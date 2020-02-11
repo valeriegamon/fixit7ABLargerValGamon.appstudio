700d34434ec0da6d4b2036bb6fca9ba243170f5d
valeriegamon <valeriegamon@valeries-mbp.ddns.creighton.edu>
2020-02-11 14:41:28 -0600

    doneee

--- forms/friends/friends.js
+++ forms/friends/friends.js
@@ -1,4 +1,3 @@
-
 let firstFriend = prompt('Enter the name of your first of three friends.');
 let secondFriend = prompt('Enter the name of your second of three friends.');
 let thirdFriend = prompt('Enter the name of your third of three friends.');
@@ -14,8 +13,30 @@ alert(friendNames);
 
 if (bestFriend == '1') {
    alert('Your best friend is:  ' + friendNames[0]);
+       if (friendNames[0].charAt(0) == 'A' || friendNames[0].charAt(0) == 'a') {
+       alert('Your best friends" name starts with A.');
+       } else if (friendNames[0].charAt(0) == 'B' || friendNames[0].charAt(0) == 'b') {
+       alert('Your best friends" name starts with B.');
+       } else {
+       alert('Your best friend"s name starts with something other than A or B.');
+       }
   } else if (bestFriend == '2') {
    alert('Your best friend is:  ' + friendNames[1]);
-  } else if (bestFriend == '3') {
+       if (friendNames[1].charAt(0) == 'A' || friendNames[1].charAt(0) == 'a') {
+       alert('Your best friends" name starts with A.');
+       } else if (friendNames[1].charAt(0) == 'B' || friendNames[1].charAt(0) == 'b') {
+       alert('Your best friends" name starts with B.');
+       } else {
+       alert('Your best friend"s name starts with something other than A or B.');
+       }
+  } else if (bestFriend === '3') {
    alert('Your best friend is:  ' + friendNames[2]);
+        if (friendNames[2].charAt(0) === 'A' || friendNames[2].charAt(0) === 'a') {
+       alert('Your best friends" name starts with A.');
+       } else if (friendNames[2].charAt(0) === 'B' || friendNames[2].charAt(0) === 'b') {
+       alert('Your best friends" name starts with B.');
+       } else {
+       alert('Your name starts with something other than A or B.');
+       }
  } 
+ 
